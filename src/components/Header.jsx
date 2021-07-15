6745bf1b8424006eeb1911dcabfa5b08dda0ad01
mastersu83 <master_su@mail.ru>
2021-07-16 02:19:22 +0300

    react

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+	return (
+		<header className='header'>
+			<img
+				className='header__img'
+				src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/1024px-BMW_logo_%28gray%29.svg.png'
+				alt=''
+			/>
+			<div className='title'>Title</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+	return (
+		<div className='profile'>
+			<div>
+				<img
+					className='content__img'
+					src='https://7oom.ru/wp-content/uploads/peizaji-01.jpg'
+					alt=''
+				/>
+			</div>
+			<div>
+				My posts
+				<div>New post</div>
+				<div>
+					<div>post1</div>
+					<div>post2</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
