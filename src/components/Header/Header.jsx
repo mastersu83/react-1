56cc83d00eaf1659d04003edaf06db13ee8814ed
master_su <master_su@mail.ru>
2021-07-28 02:38:51 +0300

    добавил логотип из локальной папки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,13 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import logo from "../../img/logo.png";
 
 const Header = () => {
-	return (
-		<header className={classes.header}>
-			<img
-				className={classes.img}
-				src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/1024px-BMW_logo_%28gray%29.svg.png'
-				alt=''
-			/>
-			<div className='title'>Title</div>
-		</header>
-	);
+  return (
+    <header className={classes.header}>
+      <img className={classes.img} src={logo} alt="" />
+      <div className="title">Title</div>
+    </header>
+  );
 };
 
 export default Header;
