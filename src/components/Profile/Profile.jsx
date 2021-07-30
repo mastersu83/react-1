d80109f77d378789d91e77e5e399447f9abd5eec
master_su <master_su@mail.ru>
2021-07-31 02:25:01 +0300

    добавил вместо тега ссылки "а" тег NavLink

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,18 +2,18 @@ import MyPosts from "./MyPosts/MyPosts";
 import classes from "./Profile.module.css";
 
 const Profile = () => {
-	return (
-		<div >
-			<div>
-				<img
-					className={classes.img}
-					src="https://7oom.ru/wp-content/uploads/peizaji-01.jpg"
-					alt=""
-				/>
-			</div>
-			<MyPosts message="hello" />
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <img
+          className={classes.img}
+          src="https://7oom.ru/wp-content/uploads/peizaji-01.jpg"
+          alt=""
+        />
+      </div>
+      <MyPosts message="hello" />
+    </div>
+  );
 };
 
 export default Profile;
