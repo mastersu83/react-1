ef4090a00cbdf23386246c782d8bc3d2af843eb0
Master_su <master_su@mail.ru>
2021-10-25 02:40:30 +0300

    62 - React JS - практика, follow-unfollow api У GET и DELETE - это должно передаваться 2-ым параметром, у POST - 3-тьим

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Users.module.css";
 import userPhoto from "../../assets/images/headache.png";
 import { NavLink } from "react-router-dom";
+import * as axios from "axios";
 
 const Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -44,20 +45,51 @@ const Users = (props) => {
                 {u.followed ? (
                   <button
                     onClick={() => {
-                      props.unfollow(u.id);
-                    }}
-                  >
-                    follow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      props.follow(u.id);
+                      axios
+                        .delete(
+                          `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+
+                          {
+                            withCredentials: true,
+                            headers: {
+                              "API-KEY": "8adacebe-0ada-4cf1-a6f3-ff0b4c1f9fde",
+                            },
+                          }
+                        )
+                        .then((response) => {
+                          if (response.data.resultCode === 0) {
+                            props.unfollow(u.id);
+                          }
+                        });
                     }}
                   >
                     unfollow
                   </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      axios
+                        .post(
+                          `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                          {},
+                          {
+                            withCredentials: true,
+                            headers: {
+                              "API-KEY": "8adacebe-0ada-4cf1-a6f3-ff0b4c1f9fde",
+                            },
+                          }
+                        )
+                        .then((response) => {
+                          if (response.data.resultCode === 0) {
+                            props.follow(u.id);
+                          }
+                        });
+                    }}
+                  >
+                    follow
+                  </button>
                 )}
+                )
               </div>
             </span>
             <span>
