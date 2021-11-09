a0570489a29a52fb18056e0793ded2fb47845943
master_su <master_su@mail.ru>
2021-11-10 02:18:34 +0300

    71 - React JS - setState, local state

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,41 +1,43 @@
-import React from 'react';
-import classes from './ProfileInfo.module.css';
-import Preloader from '../../common/Preloader/Preloader';
-import userPhoto from '../../../assets/images/headache.png';
+import React from "react";
+import classes from "./ProfileInfo.module.css";
+import Preloader from "../../common/Preloader/Preloader";
+import userPhoto from "../../../assets/images/headache.png";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
-	if (!props.profile) {
-		return <Preloader />;
-	}
-	return (
-		<div>
-			<img
-				className={classes.img}
-				src='https://7oom.ru/wp-content/uploads/peizaji-01.jpg'
-				alt=''
-			/>
-			<div>
-				<img
-					src={
-						props.profile.photos.large != null
-							? props.profile.photos.small
-							: userPhoto
-					}
-					alt=''
-					className={classes.userPhoto}
-				/>
-				<div>{props.profile.fullName}</div>
-			</div>
-			<div>
-				<h3>Контакты</h3>
-				<div>Facebook: {props.profile.contacts.facebook}</div>
-				<div>Вконтакте: {props.profile.contacts.vk}</div>
-				<div>Twitter: {props.profile.contacts.twitter}</div>
-				<div>Instagram: {props.profile.contacts.instagram}</div>
-				<div>Github: {props.profile.contacts.github}</div>
-			</div>
-		</div>
-	);
+  if (!props.profile) {
+    return <Preloader />;
+  }
+  return (
+    <div>
+      {/*<img*/}
+      {/*  className={classes.img}*/}
+      {/*  src="https://7oom.ru/wp-content/uploads/peizaji-01.jpg"*/}
+      {/*  alt=""*/}
+      {/*/>*/}
+      <div>
+        <img
+          src={
+            props.profile.photos.large != null
+              ? props.profile.photos.small
+              : userPhoto
+          }
+          alt=""
+          className={classes.userPhoto}
+        />
+        <ProfileStatus status={"hello"} />
+        <div>{props.profile.fullName}</div>
+      </div>
+      <div>
+        <h3>Контакты</h3>
+        <div>Facebook: {props.profile.contacts.facebook}</div>
+        <div>Вконтакте: {props.profile.contacts.vk}</div>
+        <div>Twitter: {props.profile.contacts.twitter}</div>
+        <div>Instagram: {props.profile.contacts.instagram}</div>
+        <div>Github: {props.profile.contacts.github}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileInfo;
