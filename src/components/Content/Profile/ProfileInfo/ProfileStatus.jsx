cfde22b79a26fa48050bf4dd80e150603be0e238
master <master_su@mail.ru>
2021-12-01 00:23:21 +0300

    74 - React JS - componentDidUpdate

--- src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileStatus.jsx
@@ -23,7 +23,15 @@ class ProfileStatus extends React.Component {
     this.setState({ status: text });
   };
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({ status: this.props.status });
+    }
+    console.log("componentDidUpdate");
+  }
+
   render() {
+    console.log("render");
     return (
       <div>
         {!this.state.editMode && (
