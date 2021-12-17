9bb6d9f3876619122e272c5d39d4870e2e346288
master_su <master_su@mail.ru>
2021-12-18 00:36:44 +0300

    81 - React JS - селекторы (reselect part 1)

--- src/components/Content/Users/UsersContainer.jsx
+++ src/components/Content/Users/UsersContainer.jsx
@@ -9,6 +9,14 @@ import Users from "./Users";
 import Preloader from "../../common/Preloader/Preloader";
 import { compose } from "redux";
 import { withAuthRedirect } from "../../../hoc/withAuthRedirect";
+import {
+  getCurrentPageSelector,
+  getFollowingInProgressSelector,
+  getIsFetchingSelector,
+  getPageSizeSelector,
+  getTotalUsersCountSelector,
+  getUsersSelector,
+} from "../../../Redux/users_selectors";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
@@ -42,12 +50,12 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress,
+    users: getUsersSelector(state),
+    pageSize: getPageSizeSelector(state),
+    totalUsersCount: getTotalUsersCountSelector(state),
+    currentPage: getCurrentPageSelector(state),
+    isFetching: getIsFetchingSelector(state),
+    followingInProgress: getFollowingInProgressSelector(state),
   };
 };
 
